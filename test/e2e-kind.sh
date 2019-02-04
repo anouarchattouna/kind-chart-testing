bc21b0d0070ff063be67f796b19bef5b7eea7e20
Anouar Chattouna <anouar.chattouna@gmail.com>
2019-02-04 10:14:09 +0100

    Update kind installation

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -37,16 +37,17 @@ create_kind_cluster() {
     curl -sSLo kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64"
     chmod +x kind
     sudo mv kind /usr/local/bin/kind
+    chown root:root kind
     echo '/usr/local/bin/ : '
     ls -ltr /usr/local/bin/
 
-    kind create cluster --name "$CLUSTER_NAME" --config test/kind-config.yaml --image "kindest/node:$K8S_VERSION"
+    /usr/local/bin/kind create cluster --name "$CLUSTER_NAME" --config test/kind-config.yaml --image "kindest/node:$K8S_VERSION"
 
     docker_exec mkdir -p /root/.kube
 
     echo 'Copying kubeconfig to container...'
     local kubeconfig
-    kubeconfig="$(kind get kubeconfig-path --name "$CLUSTER_NAME")"
+    kubeconfig="$(/usr/local/bin/kind get kubeconfig-path --name "$CLUSTER_NAME")"
     docker cp "$kubeconfig" ct:/root/.kube/config
 
     docker_exec kubectl cluster-info
